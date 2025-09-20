41d807eea613860e95b3cd6db48e3ac6ef2a13f1
waleed7elmy <160430781+waleed7elmy@users.noreply.github.com>
2025-09-20 13:30:50 +0300

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS helmy_auth;
+USE helmy_auth;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (username, email, password_hash) VALUES 
+('admin', 'admin@helmy.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'); -- password: password
